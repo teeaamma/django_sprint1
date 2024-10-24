25b8e0f76ca2197a3d7153c37f6f3b279353bbf8
Timofey Chervov <chervov.timofey@yandex.ru>
2024-10-24 16:28:37 +0700

    13212

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'blog'
@@ -8,7 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('posts/<int:id>/', views.post_detail, name='post_detail'),
     path(
-        'category/<slug:category_slug>/', 
+        'category/<slug:category_slug>/',
         views.category_posts, name='category_posts'
     ),
 ]
